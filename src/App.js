a89027164156c7a9895323bd310f1a8905b2ca56
vo1200 <vanessaoru@yahoo.com>
2023-02-04 18:57:44 -0500

    completed basic logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+let log = console.log;
+
+const Folder = (props) => {
+  const { name, children } = props;
+  log("in folder props", props);
+
+  const folderStyles = { border: "2px solid pink" };
+  const fileStyles = { marginLeft: "20px", border: "2px solid green" };
+
+  const [isOpen, setOpen] = useState(false);
+  const caretDirection = isOpen ? "down" : "right";
+  const handleClick = () => {
+    setOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <div style={folderStyles}>
+        <span onClick={handleClick}>
+          <i className="blue folder icon"></i>
+          <i className={`caret ${caretDirection} icon`}></i>
+          {name}
+        </span>
+        <div style={fileStyles}>{isOpen ? children : null}</div>
+      </div>
+    </>
+  );
+};
+
+const File = (props) => {
+  const { name } = props;
+  const fileExt = name.split(".")[1];
+  const fileIcons = {
+    mp4: "headphones",
+    png: "file",
+    jpeg: "file outline",
+    js: "code",
+    css: "paint brush",
+  };
+  return (
+    <>
+      <div>
+        <span>
+          <i className={`pink ${fileIcons[fileExt]} icon`}></i>
+        </span>
+        {name}
+      </div>
+    </>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Folder name="Desktop">
+        <Folder name="Pet Memes">
+          <File name="dog-memes.png" />
+          <File name="cat-memes.jpeg" />
+        </Folder>
+      </Folder>
+      <Folder name="Music">
+        <File name="fav-song.mp4" />
+        <File name="classical.mp4" />
+      </Folder>
+      <Folder name="Application">
+        <File name="hello-world.js" />
+        <File name="first-styles.css" />
+      </Folder>
+    </>
   );
 }
 
